6a14829b20c93565ffb94a1efb980837795a9631
Hunter <huntersfred@gmail.com>
2023-02-24 20:23:46 -0500

    Added Gate Scripts

---
+++ Assets/_Scripts/Events/ChangeColor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/_Scripts/Objects/GateObject.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GateObject : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
+}
